b8e677860646a4d27493e28ad11153617b49ee5a
Joshua Ssemwanga <93733532+jssem001@users.noreply.github.com>
2025-03-23 13:05:09 +0300

    ready to deploy

--- my-app/src/aboutme.jsx
+++ my-app/src/aboutme.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 // import { motion } from "framer-motion";
 import { Sun, Moon } from "lucide-react";
